7daabe53c75291e3c89c0c039f9ff1ad39559725
UF <UF@DESKTOP-1K98S3N>
2017-11-21 19:11:43 +0200

    all classes are update now

--- Matala_0/Data.java
+++ Matala_0/Data.java
@@ -1,8 +1,14 @@
 package Matala_0;
 
+
 import java.io.FileReader;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 /**
  * This class prepares data from a csv type file for the WriteKml class to write.
@@ -10,18 +16,21 @@ import java.util.Scanner;
  *
  */
 public class Data {
-
 	ArrayList<String[]> arr;
+	String path;
 
-	public Data(){
+	public Data(String path){
 		arr= new ArrayList<String[]>();
+		this.path=path;
 	}
 
 	/**
 	 * Loads all the data from the pathfile.
 	 * @param pathFile
 	 */
-	public void loadAllFromFile(String pathFile){
+	public WriteKml loadAllFromFile(){
+		String pathFile=this.path;
+		WriteKml arbel=new WriteKml();
 		try{
 			Scanner scanner=new Scanner(new FileReader(pathFile));
 			String line;
@@ -37,7 +46,6 @@ public class Data {
 
 				record= new lineData(time, lat, lon);
 
-
 				arr.add(results);
 
 			}
@@ -51,6 +59,8 @@ public class Data {
 			i++;
 		}
 		System.out.println(i);
+		arbel.setArr(arr);
+		return arbel;
 	}
 	/**
 	 * Receives a longitude and latitude coordinate and loads the data from the rows ,in the csv type file, within a certain radius.  
@@ -58,11 +68,12 @@ public class Data {
 	 * @param user_lon 
 	 * @param user_lat
 	 */
-	public void nearFilter(String pathFile, double user_lon, double user_lat){
-		double dist=0.001;
+	public WriteKml FilterByDist(double user_lat, double user_lon, double dist_by_meter){
+		WriteKml arbel=new WriteKml();
+		String pathFile=this.path;
 		try{
 			Scanner scanner=new Scanner(new FileReader(pathFile));
-
+			
 			String line;
 			lineData ld;
 			scanner.nextLine();
@@ -75,10 +86,13 @@ public class Data {
 				String time=results[0];
 				ld= new lineData(time, lat, lon);
 
-				double check=Math.abs(user_lon-ld.getLon());
-				//System.out.println("check= : "+check);
-				if(Math.abs(user_lon-ld.getLon())<dist && ld.getLon()!=0)
+				double distance_from_me=distance(user_lat, lat, user_lon, lon, 0, 0);
+				System.out.println("distance from me: "+distance_from_me);
+				
+				if(distance_from_me<dist_by_meter&&distance_from_me!=0) {
 					arr.add(results);
+					
+				}
 			}
 			scanner.close();
 		}catch (Exception e){
@@ -90,14 +104,18 @@ public class Data {
 			i++;
 
 		}
-		System.out.println(i);
+		//System.out.println(i);
+		arbel.setArr(arr);
+		return arbel;
 	}
 	/**
 	 * Receives a user name and loads the data from the rows,in the csv type file, with that user name. 
 	 * @param pathFile
 	 * @param user_name
 	 */
-	public void nameFilter(String pathFile, String user_name){
+	public WriteKml filterByName(String user_name){
+		WriteKml arbel=new WriteKml();
+		String pathFile=this.path;
 		try{
 			Scanner scanner=new Scanner(new FileReader(pathFile));
 			String line;
@@ -129,6 +147,114 @@ public class Data {
 
 		}
 		System.out.println(i);
+		arbel.setArr(arr);
+		return arbel;
 	}
+	public WriteKml FilterGiveAllAfterTime(Date user_date){
+		WriteKml arbel=new WriteKml();
+		String pathFile=this.path;
+		try{
+			Scanner scanner=new Scanner(new FileReader(pathFile));
+
+			String line;
+			lineData ld;
+			scanner.nextLine();
+			while(scanner.hasNextLine()){
+				line=scanner.nextLine(); //get the line
+				//System.out.println("line"+line);
+				String []results=line.split(",");	//split the line
+				double lon=Double.parseDouble(results[3]);
+				double lat=Double.parseDouble(results[2]);
+				String time=results[0];
+				ld= new lineData(time, lat, lon);
+	
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				Date wifi_date=format.parse(ld.getTime());
+				System.out.println("wifi :"+wifi_date);
+				System.out.println("user :"+user_date);
+				if(wifi_date.before(user_date))
+					arr.add(results);
+			}
+			scanner.close();
+		}catch (Exception e){
+			System.out.println("Error: "+ e.getMessage());
+		}
+		int i=0;
+		for (String[] temp : arr) {
+			System.out.println(Arrays.toString(temp));
+			i++;
+
+		}
+		System.out.println(i);
+		arbel.setArr(arr);
+		return arbel;
+	}
+	public WriteKml FilterGiveAllBeforeTime(Date user_date){
+		WriteKml arbel=new WriteKml();
+		String pathFile=this.path;
+		try{
+			Scanner scanner=new Scanner(new FileReader(pathFile));
+
+			String line;
+			lineData ld;
+			scanner.nextLine();
+			while(scanner.hasNextLine()){
+				line=scanner.nextLine(); //get the line
+				//System.out.println("line"+line);
+				String []results=line.split(",");	//split the line
+				double lon=Double.parseDouble(results[3]);
+				double lat=Double.parseDouble(results[2]);
+				String time=results[0];
+				ld= new lineData(time, lat, lon);
+	
+				DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				Date wifi_date=format.parse(ld.getTime());
+				System.out.println("wifi :"+wifi_date);
+				System.out.println("user :"+user_date);
+				if(wifi_date.after(user_date))
+					arr.add(results);
+			}
+			scanner.close();
+		}catch (Exception e){
+			System.out.println("Error: "+ e.getMessage());
+		}
+		int i=0;
+		for (String[] temp : arr) {
+			System.out.println(Arrays.toString(temp));
+			i++;
+
+		}
+		System.out.println(i);
+		arbel.setArr(arr);
+		return arbel;
+	}
+	/**
+	 * Calculate distance between two points in latitude and longitude taking
+	 * into account height difference. If you are not interested in height
+	 * difference pass 0.0. Uses Haversine method as its base.
+	 * 
+	 * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+	 * el2 End altitude in meters
+	 * @returns Distance in Meters
+	 */
+	public static double distance(double lat1, double lat2, double lon1, double lon2, double el1, double el2) {
+
+		final int R = 6371; // Radius of the earth
+
+		double latDistance = Math.toRadians(lat2 - lat1);
+		double lonDistance = Math.toRadians(lon2 - lon1);
+		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+				+ Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+				* Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double distance = R * c * 1000; // convert to meters
+
+		double height = el1 - el2;
+
+		distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+		return Math.sqrt(distance);
+	}
+
 }
 
--- Matala_0/Main.java
+++ Matala_0/Main.java
@@ -1,8 +1,13 @@
 package Matala_0;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Folder;
 import de.micromata.opengis.kml.v_2_2_0.Icon;
@@ -10,14 +15,24 @@ import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
 import de.micromata.opengis.kml.v_2_2_0.Style;
 
+
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
-	
-		WriteKml.WriteKml("C:/Users/Levi/Desktop/Year 2/workspace_eclipse/OOP_Work/MyTrack.csv");
+	public static void main(String[] args) throws FileNotFoundException, ParseException {
 
-System.out.println("hello again again");
-		
+		String path="C:\\Users\\UF\\eclipse-workspace\\EX_0_and_1\\MyTrack_4.csv";
+		String string_date ="27-10-2017  16:16:40";
+		Data a=new Data(path);
+
+		SimpleDateFormat format= new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date date_user_want = format.parse(string_date);
+
+		a.FilterGiveAllBeforeTime(date_user_want).WriteKml();
+//		a.FilterGiveAllAfterTime(date_user_want);
+//		a.loadAllFromFile().WriteKml();
+//		a.FilterByDist(32.16866695,34.81322274, 200).WriteKml();
+//		a.filterByName("Maxillent").WriteKml();
+
 	}
 }
 
