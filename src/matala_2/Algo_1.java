01f120f84d60202a806859b7986b92c4b654d4b7
UF <UF@DESKTOP-1K98S3N>
2017-12-20 15:29:14 +0200

    added new object myHash that can add simply arraylist of values

--- src/matala_2/Algo_1.java
+++ src/matala_2/Algo_1.java
@@ -7,15 +7,14 @@ import Matala_0.Readable;
 public class Algo_1 implements Readable {
 	
 	 HashMap<String,Point_And_Sig> hm=new HashMap<String,Point_And_Sig>(); 
-/*
- * (non-Javadoc)
- * @see Matala_0.Readable#read(java.lang.String)
- */
+
 	@Override
 	public void read(String path) {
 		
 		
 	}
+	
+	
 	public void calculate() {
 		
 	}
---
+++ src/matala_2/myHash.java
@@ -0,0 +1,27 @@
+package matala_2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class myHash {
+	
+	HashMap<String,ArrayList<Point_And_Sig>> hm=new HashMap<String,ArrayList<Point_And_Sig>>(); 
+	
+	public void add(String mac,Point_And_Sig pas) {
+		boolean ifcontain = hm.containsKey(mac);
+		
+		if(ifcontain==true) {		//the key is exist
+			ArrayList<Point_And_Sig> arr=new ArrayList<Point_And_Sig>();
+			arr=hm.get(mac);
+			arr.add(pas);
+			hm.put(mac, arr);
+			
+			
+		}else {
+			ArrayList<Point_And_Sig> arr=new ArrayList<Point_And_Sig>();
+			arr.add(pas);
+			hm.put(mac, arr);
+		}
+		
+	}
+}
