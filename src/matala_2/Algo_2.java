fa17d4a436f08f1d581893582f01b52bfde48f2f
Levi <levidworkin@gmail.com>
2017-12-24 17:17:04 +0200

    Added Algo_2.

---
+++ src/matala_2/Algo_2.java
@@ -0,0 +1,36 @@
+package matala_2;
+import java.io.FileReader;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.Set;
+
+import Matala_0.Line_46;
+import Matala_0.Readable;
+import Matala_0.lineData;
+
+public class Algo_2 implements Readable{
+
+	ArrayList<Line_46> arrNoGps = new ArrayList<Line_46>();
+	ArrayList<Line_46> combo = new ArrayList<Line_46>();
+	myHash mh = new myHash();
+	
+	@Override
+	public void read(String filename) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
