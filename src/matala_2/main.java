e1589ec2f62609d5249320d55892a48961f1d13c
Levi <levidworkin@gmail.com>
2017-12-25 09:55:32 +0200

    -

--- src/matala_2/main.java
+++ src/matala_2/main.java
@@ -7,12 +7,13 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String filename = "C:\\Users\\Levi\\Desktop\\Year 2\\OOP\\Assignments\\matala 2\\testing\\_comb_all_BM2_.csv";
-		Algo_1 a = new Algo_1();
-		a.read(filename);
-		a.calculate();
-		a.write();
-	
+		String filename = "C:\\Users\\Levi\\Desktop\\Year 2\\OOP\\Assignments\\matala 2\\testing\\_comb_all_BM3_.csv";
+//		Algo_1 a = new Algo_1();
+//		a.read(filename);
+//		a.calculate();
+//		a.write();
+
+		
 	}
 
 }
